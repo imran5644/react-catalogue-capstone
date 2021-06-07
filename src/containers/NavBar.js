cf1a070c3257875fadec0c0b7466cb873e53d311
imran5644 <imi56444@gmail.com>
2021-06-08 00:44:48 +0800

    Adds react-router-dom package, components and containers, Creates Routes on index.js, Adds links on the navigation bar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav>Here is the navigation bar</nav>
-  )
+    <nav>
+      Here is the navigation bar
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
