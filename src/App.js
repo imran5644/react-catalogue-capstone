36149e3b4d83384f65081c9688db525a5ee7a693
imran5644 <imi56444@gmail.com>
2021-06-08 00:52:13 +0800

    Adds linter files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App = () => {
-  return (
-      <div className="App">
-        <p>Basic structure</p>
-      </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <p>Basic structure</p>
+  </div>
+);
 
 export default App;
