8d05c3dbf91b6f7ade11f39bcee168509bf0c06c
imran5644 <imi56444@gmail.com>
2021-06-09 23:34:31 +0800

    Add copyright on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../stylesheets/Footer.css';
 
 const Footer = () => (
-  <footer className="p-5">Here is the footer</footer>
+  <footer className="p-5">&copy; 2021 - Imran Yousaf</footer>
 );
 
 export default Footer;
