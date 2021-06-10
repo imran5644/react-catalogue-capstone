d4c12ead4b4ea8ae104b52567d7e2b8fc6d40fca
imran5644 <imi56444@gmail.com>
2021-06-11 05:56:15 +0800

    Fix linter errors

--- src/reducers/rootReducer.test.js
+++ src/reducers/rootReducer.test.js
@@ -15,16 +15,14 @@ describe('dogsReducer', () => {
     ];
 
     it('should return an array contains a single item that its name equals the name of the item inside the action categories', () => {
-      expect(dogsReducer(items, {loading:true})).toEqual([{ name: 'Dogs playing' }]);
+      expect(dogsReducer(items, { loading: true })).toEqual([{ name: 'Dogs playing' }]);
     });
-
 
     describe('FETCH DOGS FAILURE', () => {
       it('should return false inside the action url', () => {
         expect(dogsReducer(false, {})).toBeFalsy();
       });
     });
-
 
     describe('FETCH DOGS SUCCESS', () => {
       it('should return an array containing items inside the action url', () => {
