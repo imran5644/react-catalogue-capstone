9e814e98add1d1fd664effb7d0bf14552f7d40b0
imran5644 <imi56444@gmail.com>
2021-06-11 00:47:30 +0800

    Fix linters Errors

--- src/containers/thunkMock.js
+++ src/containers/thunkMock.js
@@ -1,19 +1,19 @@
-export const thunk = ({ dispatch, getState }) => next => action => {
-    if (typeof action === 'function') {
-      return action(dispatch, getState);
-    }
-  
-    return next(action);
+export const thunk = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action === 'function') {
+    return action(dispatch, getState);
+  }
+
+  return next(action);
+};
+
+export const create = () => {
+  const store = {
+    getState: jest.fn(() => ({})),
+    dispatch: jest.fn(),
   };
-  
-  export const create = () => {
-    const store = {
-      getState: jest.fn(() => ({})),
-      dispatch: jest.fn(),
-    };
-    const next = jest.fn();
-  
-    const invoke = action => thunk(store)(next)(action);
-  
-    return { store, next, invoke };
-  };
+  const next = jest.fn();
+
+  const invoke = (action) => thunk(store)(next)(action);
+
+  return { store, next, invoke };
+};
